2cb0cf293ceb77639d2b352a959e8925c73fe3f5
codewriter3000 <amicharski@outlook.com>
2024-05-28 14:23:01 -0400

    sct is closer to being POSIX compliant

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
 import secrets
 import sys
 
-
-if __name__ == '__main__':
-    if len(sys.argv) == 0:
+def main():
+    if len(sys.argv) == 1:
         print('Usage: sct [OPTION]...')
         exit(1)
-    
+   
     args = sys.argv[1:]
-    
+
     for i in range(0, len(args) - 1):
         arg = args[i]
-
-        if arg[0] == '-':
-            if arg[1:] == 'tokenbytes':
-                print(secrets.token_bytes(int(args[i+1])))
-                exit(0)
-            elif arg[1:] == 'tokenhex':
-                print(secrets.token_hex(int(args[i+1])))
-                exit(0)
-            elif arg[1:] == 'tokenurlsafe':
-                print(secrets.token_urlsafe(int(args[i+1])))
-                exit(0)
+        
+        if arg[0:2] == '--':
+            if arg[2:] == 'tokenbytes':
+                token_bytes(args[i+1])
+            elif arg[2:] == 'tokenhex':
+                token_hex(args[i+1])
+            elif arg[2:] == 'tokenurlsafe':
+                token_urlsafe(args[i+1])
         else:
             print('Invalid syntax')
             exit(2)
 
 
-    print(args)
+def token_bytes(size):
+    print(secrets.token_bytes(int(size)))
+
+
+def token_hex(size):
+    print(secrets.token_hex(int(size)))
+
+
+def token_urlsafe(size):
+    print(secrets.token_urlsafe(int(size)))
+
+
+if __name__ == '__main__':
+   main()
 
